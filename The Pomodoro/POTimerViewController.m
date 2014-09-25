995b1329ecd86cc97d4638bf3ce1f3d4f83e1880
Ben Norris <ben@bsn.io>
2014-09-25 12:22:52 -0600

    Added progress circle around timer to indicate time being passed

--- The Pomodoro/POTimerViewController.m
+++ The Pomodoro/POTimerViewController.m
@@ -8,6 +8,7 @@
 
 #import "POTimerViewController.h"
 #import "POTimer.h"
+#import "POCircleProgressView.h"
 
 #define margin 15
 
@@ -15,6 +16,7 @@
 
 @property (nonatomic, strong) UILabel *timeLabel;
 @property (nonatomic, strong) UIButton *timerButton;
+@property (nonatomic, strong) POCircleProgressView *progressView;
 
 @end
 
@@ -37,18 +39,31 @@
     self.title = @"Timer";
     self.view.backgroundColor = [UIColor redColor];
     
-    self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, 100, self.view.bounds.size.width - (margin * 2), 100)];
+    self.progressView = [[POCircleProgressView alloc] initWithFrame:CGRectMake(margin, margin * 2, self.view.bounds.size.width - (margin * 2), self.view.bounds.size.width - (margin * 2))];
+    self.progressView.fillColor = [UIColor whiteColor];
+    self.progressView.fillBackgroundColor = nil;
+    self.progressView.borderColor = nil;
+    [self.view addSubview:self.progressView];
+    
+    CGFloat progressBorder = 5;
+    POCircleProgressView *blockingCircle = [[POCircleProgressView alloc] initWithFrame:CGRectMake(self.progressView.frame.origin.x + progressBorder, self.progressView.frame.origin.y + progressBorder, self.progressView.frame.size.width - (progressBorder * 2), self.progressView.frame.size.height - (progressBorder * 2))];
+    blockingCircle.fillBackgroundColor = [UIColor redColor];
+    blockingCircle.borderColor = nil;
+    [self.view addSubview:blockingCircle];
+    
+    CGFloat labelHeight = 90;
+    CGFloat labelVerticalPosition = self.progressView.frame.origin.y + (self.progressView.bounds.size.height / 2) - (labelHeight / 2);
+    self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, labelVerticalPosition, self.view.bounds.size.width - (margin * 2), labelHeight)];
     self.timeLabel.textColor = [UIColor whiteColor];
-    self.timeLabel.font = [UIFont fontWithName:@"Avenir Next" size:100];
+    self.timeLabel.font = [UIFont fontWithName:@"Avenir Next" size:labelHeight];
     self.timeLabel.textAlignment = NSTextAlignmentCenter;
     [self updateLabel];
-    
     [self.view addSubview:self.timeLabel];
     
-    self.timerButton = [[UIButton alloc] initWithFrame:CGRectMake(margin, 250, self.view.bounds.size.width - (margin * 2), 30)];
+    CGFloat buttonVerticalPosition = self.progressView.frame.origin.y + self.progressView.bounds.size.height + margin;
+    self.timerButton = [[UIButton alloc] initWithFrame:CGRectMake(margin, buttonVerticalPosition, self.view.bounds.size.width - (margin * 2), 30)];
     [self.timerButton setTitle:@"Start round" forState:UIControlStateNormal];
     [self.timerButton addTarget:self action:@selector(startSession) forControlEvents:UIControlEventTouchUpInside];
-    
     [self.view addSubview:self.timerButton];
 }
 
@@ -70,6 +85,12 @@
         self.timeLabel.text = [NSString stringWithFormat:@"%ld:%ld", minutes, seconds];
     } else {
         self.timeLabel.text = [NSString stringWithFormat:@"%ld:0%ld",minutes, seconds];
+    }
+    
+    if (([POTimer sharedInstance].seconds > 0) && ([POTimer sharedInstance].startingSeconds != [POTimer sharedInstance].seconds)) {
+        self.progressView.progress = 1 - ([POTimer sharedInstance].seconds / (CGFloat)[POTimer sharedInstance].startingSeconds);
+    } else {
+        self.progressView.progress = 0;
     }
 }
 
